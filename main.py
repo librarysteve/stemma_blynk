8bd131673e0637b546e56e1c736b45fc234f40b1
LibrarySteve <sgarrison@lexpublib.org>
2019-10-07 16:32:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+## Local Server Instructions ##
+'''
+If you're using a local server you can find the 
+Auth Token in the server management interface. 
+By default the web interface runs on port 9443
+
+Initializing an an instance of blynk depends on 
+if you're using a local or managed server. 
+For a local server uncomment the line below and 
+remove the second initialization definition.
+By default the blynk service runs on port 8080.
+
+To use the app with a local server instance, 
+Open the app, hit the log in button, then click 
+on the three dot icon just above the "Next" button
+This will allow you to select between using the
+default(managed/paid) server and the free local one
+
+The default username:admin@blynk.cc
+The default password:admin
+'''
 import busio
 import blynklib
 import blynktimer
@@ -5,40 +26,39 @@ from time import sleep
 from board import SCL, SDA
 from adafruit_seesaw.seesaw import Seesaw
 
-AUTH = '<INSERT API KEY HERE>'
-
-# If you're using a local server or one that you
-# run elsewhere, uncomment the first option.
-
-#blynk = blynklib.Blynk(AUTH, server='<SERVER IP HERE>', port='8080') # Port 8080 is the default
-                                                                      # use which ever port matched your configuration
-# Remove this line if you run your own server. 
-blynk = blynklib.Blynk(AUTH)
+AUTH = '<INSERT API TOKEN HERE>' # Can be found in the app
 
 
+## Initialize a blynk instance ##
+# blynk = blynklib.Blynk(AUTH, server='<SERVER IP HERE>', port='<SERVER PORT HERE')
+blynk = blynklib.Blynk(AUTH) # Remove this line if you run your own server. 
+
+
+## Define the sensor and protocol ##
 bus = busio.I2C(SCL, SDA)
 ss = Seesaw(bus, addr=0x36)
 timer = blynktimer.Timer()
 
 # Function to read the temp data and write it to blynk
 def read_temp(vpin_num=0):
-    c_temp = ss.get_temp() # Read temp data, in celcious by default
-    l_temp = (c_temp * 1.8) + 32 # Convert to fahrenheit
-    temp = round(l_temp, 2) # Round to 2 decemal places
-    print("Temp {}".format(temp)) # print to terminal on the device attached to the stemma
-    blynk.virtual_write(vpin_num, temp) # send the data to blynk server/app
+    c_temp = ss.get_temp()              # Define temp in celsius
+    l_temp = (c_temp * 1.8) + 32        # Convert to fahrenheit, it's celsius by default
+    temp = round(l_temp, 2)             # Round to 2 decemal places
+    print("Temp {}".format(temp))       # Output message format
+    blynk.virtual_write(vpin_num, temp) # Send the data to blynk server
 
 # Function to read moisture data and write it to blynk
 def read_moisture(vpin_num=1):
-    moisture = ss.moisture_read()
-    print("Moisture: {}".format(moisture))
-    blynk.virtual_write(vpin_num, moisture)
+    moisture = ss.moisture_read()           # Define moisture
+    print("Moisture: {}".format(moisture))  # Output message format, moisture range 0-2000
+    blynk.virtual_write(vpin_num, moisture) # Send data to blynk server
 
-@timer.register(vpin_num=2, interval=3, run_once=False) # Timer sets the interval between each data read.
-def main(vpin_num=2):                                   # This is set to 3 seconds!
-    read_moisture()
+
+@timer.register(vpin_num=2, interval=3, run_once=False) # Interval sets the time between sample readings
+def main(vpin_num=2):                                   # The timer will run this function every three seconds
+    read_moisture() # Run the the functions from above
     read_temp()
 
 while True:
-    blynk.run()
-    timer.run()
+    blynk.run() # Run the blynk service
+    timer.run() # Use the timer to set sample intervals
